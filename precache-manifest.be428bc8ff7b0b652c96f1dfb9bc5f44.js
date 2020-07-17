89a7d79de2e76eba114d0ce77a2184f903bc8330
ASHUTOSH KUMAR <kr.ashu456@gmail.com>
2020-07-18 04:07:36 +0530

    Updates

---
+++ precache-manifest.be428bc8ff7b0b652c96f1dfb9bc5f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6560020985b1a2097f00fae0a294157a",
+    "url": "/yt-clone/index.html"
+  },
+  {
+    "revision": "cc36c7ba01a464745a9d",
+    "url": "/yt-clone/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "399f992d4dde0666683e",
+    "url": "/yt-clone/static/js/2.e20d38c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/yt-clone/static/js/2.e20d38c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc36c7ba01a464745a9d",
+    "url": "/yt-clone/static/js/main.3972efc6.chunk.js"
+  },
+  {
+    "revision": "33125506b321aa08b564",
+    "url": "/yt-clone/static/js/runtime-main.5538a53a.js"
+  }
+]);
